521c08f0e07c915b2d6df2ab5512a87a20e53de1
DilrajS <49254698+DilrajS@users.noreply.github.com>
2021-03-24 13:58:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ import Player
 import time
 import random
 
+# decide how many players
+# create that many players
+# distribute initial cards to dealer & players
+# choose hit of hold
+# compare scores
