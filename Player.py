3a23414a17ddf3263c8cdc2838a55675e3183682
DilrajS <49254698+DilrajS@users.noreply.github.com>
2021-03-25 19:08:25 -0700

    update

--- Player.py
+++ Player.py
@@ -2,3 +2,6 @@ class Player:
 
     def __init__(self, score):
         self.score = score
+
+    def get_score(self):
+        return self.score
--- main.py
+++ main.py
@@ -4,8 +4,28 @@ import Player
 import time
 import random
 
-# decide how many players
+# Asks User for input on number of players.
+inputCheck = False
+while not inputCheck:
+    try:
+        numOfPlayers = int((input("How many players are going to play (0 - 3)? : \n")))
+        if type(numOfPlayers) == int:
+            inputCheck = True
+    except ValueError:
+        print("Incorrect input, try again.")
+
+# Caps number of players.
+if numOfPlayers > 3:
+    numOfPlayers = 3
+
+print(numOfPlayers)
+
 # create that many players
+playerNames = ["player1", "player2", "player3"]
+for x in range(numOfPlayers):
+    print(playerNames[x])
+    playerNames[x] == Player(0)
+
 # distribute initial cards to dealer & players
 # choose hit of hold
 # compare scores
